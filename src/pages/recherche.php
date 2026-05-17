ef8a8dfbd6f5c6c618b5623f2827a886884586c9
unknown <anisziane7@gmail.com>
2026-05-17 18:17:23 +0200

    page recherche + belle

--- src/pages/recherche.php
+++ src/pages/recherche.php
@@ -1,98 +1,103 @@
 <?php
+session_start();
 require_once '../includes/db.php';
 require_once '../includes/header.php';
 
-$sql = "SELECT * FROM chambre WHERE 1=1"; // WHERE pour pas casser les options
+$v = $_GET['vue'] ?? '';
+$b = $_GET['batiment'] ?? '';
+$l = $_GET['nb_lits'] ?? '';
+
+$sql = "SELECT * FROM chambre WHERE 1=1"; 
 $parametres = [];
 
-if (isset($_GET['vue'])) {
-    if ($_GET['vue'] != "") {
-        $sql = $sql . " AND type_vue = ?";
-        $parametres[] = $_GET['vue'];
-    }
+if ($v != "") {
+    $sql = $sql . " AND type_vue = ?";
+    $parametres[] = $v;
 }
 
-if (isset($_GET['batiment'])) {
-    if ($_GET['batiment'] != "") {
-        $sql = $sql . " AND batiment = ?";
-        $parametres[] = $_GET['batiment'];
-    }
+if ($b != "") {
+    $sql = $sql . " AND batiment = ?";
+    $parametres[] = $b;
 }
 
-if (isset($_GET['nb_lits'])) {
-    if ($_GET['nb_lits'] != "") {
-        $sql = $sql . " AND nb_lits >= ?";
-        $parametres[] = $_GET['nb_lits'];
-    }
+if ($l != "") {
+    $sql = $sql . " AND nb_lits >= ?";
+    $parametres[] = intval($l);
 }
 
+$sql = $sql . " ORDER BY num_chambre ASC";
 $prepa = $pdo->prepare($sql);
 $prepa->execute($parametres);
 $resultats = $prepa->fetchAll();
 ?>
 
-<main>
+<link rel="stylesheet" type="text/css" href="../css/recherche.css">
+
+<main class="conteneur-recherche">
     <h1>Rechercher une chambre</h1>
 
-    <div style="background-color: #f9f9f9; padding: 15px; border: 1px solid black;">
+    <div class="bloc-filtres">
         <form method="GET" action="recherche.php">
-            <p>
-                Vue : 
+            <div class="groupe-champ">
+                <label><b>Vue :</b></label>
                 <select name="vue">
                     <option value="">Peu importe</option>
-                    <option value="pistes">Pistes</option>
-                    <option value="parking">Parking</option>
+                    <option value="pistes" <?php if ($v == 'pistes') echo 'selected'; ?>>Pistes</option>
+                    <option value="parking" <?php if ($v == 'parking') echo 'selected'; ?>>Parking</option>
                 </select>
-            </p>
+            </div>
 
-            <p>
-                Bâtiment : 
+            <div class="groupe-champ">
+                <label><b>Bâtiment :</b></label>
                 <select name="batiment">
                     <option value="">Tous</option>
-                    <option value="A">A</option>
-                    <option value="B">B</option>
+                    <option value="A" <?php if ($b == 'A') echo 'selected'; ?>>A</option>
+                    <option value="B" <?php if ($b == 'B') echo 'selected'; ?>>B</option>
                 </select>
-            </p>
+            </div>
 
-            <p>
-                Nombre de lits : 
-                <input type="number" name="nb_lits">
-            </p>
+            <div class="groupe-champ">
+                <label><b>Nombre de lits :</b></label>
+                <input type="number" name="nb_lits" min="1" value="<?php echo htmlspecialchars($l); ?>">
+            </div>
 
-            <p>
-                <input type="submit" value="Filtrer les résultats">
-            </p>
+            <div class="groupe-bouton">
+                <input type="submit" value="Filtrer les résultats" class="btn-filtrer">
+            </div>
         </form>
     </div>
 
     <br>
 
-    <table border="1">
-        <tr>
-            <th>Numéro</th>
-            <th>Bâtiment</th>
-            <th>Vue</th>
-            <th>Lits</th>
-            <th>Action</th>
-        </tr>
-
-        <?php
-
-        if (count($resultats) > 0) {
-          
-            foreach ($resultats as $chambre) {
-                echo "<tr>";
-                echo "<td>" . $chambre['num_chambre'] . "</td>";
-                echo "<td>" . $chambre['batiment'] . "</td>";
-                echo "<td>" . $chambre['type_vue'] . "</td>";
-                echo "<td>" . $chambre['nb_lits'] . "</td>";
-                echo "<td><a href='details.php?id=" . $chambre['num_chambre'] . "'>Voir</a></td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td colspan='5'>Aucune chambre trouvée</td></tr>";
-        }
-        ?>
+    <table class="table-resultats" border="1" cellpadding="12" cellspacing="0">
+        <thead>
+            <tr bgcolor="#CCCCCC">
+                <th>Numéro</th>
+                <th>Bâtiment</th>
+                <th>Vue</th>
+                <th>Lits</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if (count($resultats) > 0): ?>
+                <?php foreach ($resultats as $chambre): ?>
+                    <tr>
+                        <td align="center"><b>Chambre <?php echo $chambre['num_chambre']; ?></b></td>
+                        <td align="center"><?php echo htmlspecialchars($chambre['batiment']); ?></td>
+                        <td>Vue sur <?php echo htmlspecialchars($chambre['type_vue']); ?></td>
+                        <td align="center"><?php echo $chambre['nb_lits']; ?> lits</td>
+                        <td align="center">
+                            <a href="details.php?id=<?php echo $chambre['num_chambre']; ?>" class="lien-voir">Voir</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="5" align="center"><i>Aucune chambre trouvée</i></td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
     </table>
 </main>
 
