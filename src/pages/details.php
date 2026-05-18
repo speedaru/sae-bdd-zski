be551a0b82f8a59df8626e91d858eadc8e58791d
speedaru <esn4fre@gmail.com>
2026-05-18 02:42:05 +0200

    feat: finialstion pages details et recherche

--- src/pages/details.php
+++ src/pages/details.php
@@ -1,6 +1,4 @@
 <?php
-session_start();
-require_once '../includes/db.php';
 require_once '../includes/header.php';
 
 $id_get = $_GET['id'] ?? 0;
@@ -14,11 +12,14 @@ $stmtFormules = $pdo->query("SELECT * FROM formule ORDER BY prix_base ASC");
 $formules = $stmtFormules->fetchAll(PDO::FETCH_ASSOC);
 
 if ($chambre == false) {
-    echo "<br><br><center>";
-    echo "<h1 style='color:red;'>ERREUR : La chambre n'existe pas !</h1>";
-    echo "<p>Le logement demandé est introuvable dans la base de données de notre station.</p>";
-    echo "<a href='recherche.php'>Retourner à la recherche</a>";
-    echo "</center>";
+    ?>
+    <link rel="stylesheet" href="../assets/css/details.css">
+    <div class="error-wrapper">
+        <h1 class="error-title">ERREUR : La chambre n'existe pas !</h1>
+        <p class="error-text">Le logement demandé est introuvable dans la base de données de notre station.</p>
+        <p><a href="recherche.php" class="btn-defaut">Retourner à la recherche</a></p>
+    </div>
+    <?php
     require_once '../includes/footer.php';
     exit();
 }
@@ -26,7 +27,6 @@ if ($chambre == false) {
 $panier = $_SESSION['panier'] ?? [];
 $is_in_panier = in_array($id_chambre, $panier);
 ?>
-
 
 <main class="conteneur-principal">
     
@@ -41,60 +41,61 @@ $is_in_panier = in_array($id_chambre, $panier);
         <div class="barre-separation"></div>
     </div>
 
-    <br><br>
-
-    <table class="table-presentation" border="1" cellpadding="20" cellspacing="0">
+    <table class="table-presentation">
         <tr>
-            <td class="colonne-technique" valign="top">
+            <td class="colonne-technique">
                 <h3 class="titre-section blue-bg">Caractéristiques techniques</h3>
-                <p><b>Bâtiment :</b> Bâtiment <?php echo htmlspecialchars($chambre['batiment']); ?></p>
-                <p><b>Étage :</b> Niveau <?php echo $chambre['etage']; ?></p>
-                <p><b>Superficie :</b> <?php echo $chambre['superficie']; ?> m²</p>
-                <p><b>Capacité d'accueil :</b> <?php echo $chambre['nb_lits']; ?> couchages individuels</p>
-                <p><b>Exposition & Panorama :</b> Vue imprenable sur <?php echo htmlspecialchars($chambre['type_vue']); ?></p>
-                <p>
-                    <b>Présence d'un balcon extérieur :</b> 
-                    <b><?php echo $chambre['balcon_present'] ? 'Oui' : 'Non'; ?></b>
-                </p>
+                <div class="tech-info-group">
+                    <p><strong>Bâtiment :</strong> Bâtiment <?php echo htmlspecialchars($chambre['batiment']); ?></p>
+                    <p><strong>Étage :</strong> Niveau <?php echo $chambre['etage']; ?></p>
+                    <p><strong>Superficie :</strong> <?php echo $chambre['superficie']; ?> m²</p>
+                    <p><strong>Capacité d'accueil :</strong> <?php echo $chambre['nb_lits']; ?> couchages individuels</p>
+                    <p><strong>Exposition & Panorama :</strong> Vue imprenable sur <?php echo htmlspecialchars($chambre['type_vue']); ?></p>
+                    <p>
+                        <strong>Présence d'un balcon extérieur :</strong> 
+                        <strong><?php echo $chambre['balcon_present'] ? 'Oui' : 'Non'; ?></strong>
+                    </p>
+                </div>
             </td>
             
-            <td class="colonne-tarifs" valign="top">
+            <td class="colonne-tarifs">
                 <h3 class="titre-section black-bg">Grille tarifaire (Semaine)</h3>
-                <table class="table-prix" border="1" cellpadding="10" cellspacing="0">
-                    <tr bgcolor="#EEEEEE">
-                        <td><b>Type de formule</b></td>
-                        <td align="right"><b>Prix de base / pers.</b></td>
-                    </tr>
-                    <?php foreach ($formules as $f): ?>
-                    <tr>
-                        <td><b><?php echo htmlspecialchars($f['type_formule']); ?></b></td>
-                        <td align="right" class="prix-bleu"><?php echo $f['prix_base']; ?> €</td>
-                    </tr>
-                    <?php endforeach; ?>
+                <table class="table-prix">
+                    <thead>
+                        <tr>
+                            <th>Type de formule</th>
+                            <th class="text-right">Prix de base / pers.</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($formules as $f): ?>
+                        <tr>
+                            <td><strong><?php echo htmlspecialchars($f['type_formule']); ?></strong></td>
+                            <td class="text-right prix-bleu"><?php echo $f['prix_base']; ?> €</td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
                 </table>
-                <br>
-                <p align="center"><i><small>Remises familles appliquées au moment de la validation finale.</small></i></p>
+                <p class="remise-notice">Remises familles appliquées au moment de la validation finale.</p>
             </td>
         </tr>
     </table>
 
-    <br><br><br>
-
     <div class="zone-boutons">
-        <a href="recherche.php"><button class="btn-defaut">Retour à la recherche</button></a>
-        &nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="recherche.php" class="btn-link">
+            <button class="btn-defaut">&larr; Retour à la recherche</button>
+        </a>
         
         <?php if ($is_in_panier): ?>
-            <a href="../actions/supprimer_panier.php?id=<?php echo $id_chambre; ?>">
+            <a href="../actions/supprimer_panier.php?id=<?php echo $id_chambre; ?>" class="btn-link">
                 <button class="btn-retirer">Retirer de ma sélection</button>
             </a>
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="reservation.php">
-                <button class="btn-valider">Finaliser ma réservation</button>
+            <a href="reservation.php" class="btn-link">
+                <button class="btn-valider">Finaliser ma réservation &rarr;</button>
             </a>
         <?php else: ?>
-            <a href="../actions/ajouter_panier.php?id=<?php echo $id_chambre; ?>">
-                <button class="btn-ajouter">Ajouter à ma sélection</button>
+            <a href="../actions/ajouter_panier.php?id=<?php echo $id_chambre; ?>" class="btn-link">
+                <button class="btn-ajouter">[+] Ajouter à ma sélection</button>
             </a>
         <?php endif; ?>
     </div>
